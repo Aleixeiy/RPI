f54cfee871ab0f8330db725b41ba886dd77b4098
Lesha <Pleskach.Lesha@yandex.ru>
2021-03-25 16:34:56 +0300

    РПИ работа 1

---
+++ index.php
@@ -0,0 +1,140 @@
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <link rel="stylesheet" href="style.css">
+            <title>rpi</title>
+        </head>
+        <body>
+            
+            <header class="header">
+                    <div class="headerLocation">
+                        <div class="headerSize">
+                            <div class="headerMenu">
+                                <a class="headerMenuItem">HOME</a>
+                                <a class="headerMenuItem">PHOTOAPP</a>
+                                <a class="headerMenuItem"><img src="./img/camera.png"></a>
+                                <a class="headerMenuItem">DESIGN</a>
+                                <a class="headerMenuItem">DOWNLOAD</a>
+                            </div>
+                        </div>
+                    </div>
+            </header>
+            
+            <main>
+                
+                <div class="first">
+                    <div class="firstLocation">
+                        <div class="firstText">
+                            <h1 class="firstTitle">your life , a photo</h1>
+                            <span class="firstAddingText">print your life in a simple photo</span>
+                            <a class="firstButton">get started</a> 
+                        </div>
+                        <div class="firstNetworks">
+                            <img class="twitter" src="./img/twitter.svg">
+                            <img class="flicker" src="./img/flicker.svg">
+                            <img class="dribble" src="./img/dribble.svg">
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="second">
+                    <div class="secondSize">
+                        <div class="secondLocation">
+                            <div>
+                                <h2 class="secondText">rapresent your life with a simple photo</h1>
+                                <p class="secondAddingText">Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br> sed do euismod tempor incididunt ut labore et dolore<br> magna aliqua. Ut <span class="secondBoldText">enim ad minim</span> veniam, quis<br> nostrud exercitation ullamco laboris nisi<br> ut aliquip ex ea commodo consequat.</p>
+                                <a class="secondButton">get started</a> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="third">
+                    <div class="thirdLocation">
+                        <div class="thirdSize">
+                            <div class="thirdRightPart">
+                                <h2 class="thirdRightPartText">design</h2>
+                                <p class="thirdRightPartAddingText">Lorem ipsum dolor sit amet, consectetur, adipiscing elit,<br> sed do euismod tempor incididunt ut labore et dolore<br> magna aliqua. Ut <span class="textboldsecond">enim ad minim</span> veniam, quis<br> nostrud exercitation ullamco laboris nisi<br> ut aliquip ex ea commodo consequat.</p>
+                                <a class="thirdButton">learn more</a> 
+                                <div class="thirdRightPartNumbers">
+                                    <p>01</p>
+                                    <p>/</p>
+                                    <p>05</p>
+                                </div>
+                                <div>
+                                    <img class="arrow1" src="./img/arrow1.png">
+                                    <img class="arrow2" src="./img/arrow2.png">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="forth">
+                    <div class="forthSize">
+                        <div class="forthLocation">
+                            <div class="forthArrows">
+                                <img class="forthLeftArrow" src="./img/arrow1.png">
+                                <img class="forthRightArrow" src="./img/arrow2.png">
+                            </div>
+                            <div>
+                                <img class="forthCamera" src="./img/camera2.svg">
+                                <h2 class="forthText">our community</h2>
+                                <p class="forthAddingText">Lorem ipsum dolor sit <br> amet, consectetur<br> adipiscing elit,</p>
+                            </div>
+                            <div class="forthPhotos">
+                                <img class="forthBus" src="./img/bus.svg">
+                                <img class="forhtLeafs" src="./img/leafs.svg">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="fifth">
+                    <div class="fifthLocation" >
+                        <div>
+                            <h2 class="fifthText" >subscribe to us community</h2>
+                        </div>
+                        <form class="fifthRow" method="get">
+                            <input type="email" class="fifthEmailField" placeholder="your email" required>
+                            <a class="fifthButton"><img src="./img/ok.png"></a> 
+                        </form>
+                    </div>
+                </div>
+                
+                <div class="sixth" >
+                    <div class="sixthLocation">
+                        <h2 class="sixthText" >download it</h2>
+                        <p class="sixthAddingText">lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do euismod tempor incididunt ut<br> labore et dolore magna aliqua. ut enim ad minim veniam, quis nostrud exercitation ullamco<br> laboris nisi ut al quip ex ea commodo consequat.</p>
+                        <div class="sixthRow">
+                        <a class="sixthApple1">apple store</a> 
+                        <a class="sixthApple2"><img src="./img/apple.png"></a> 
+                        <a class="sixthPlay1">play store</a> 
+                        <a class="sixthPlay2"><img src="./img/play.png"></a> 
+                        </div>
+                    </div>
+                </div>
+                
+            </main>
+            
+            <footer class="footer">
+                <div class="footerLocation">
+                    <div class="footerSize">
+                        <div class="footerMenu">
+                            <a class="footerMenuItem1">CREDITS</a>
+                            <a class="footerSlash">|</a>
+                            <a class="footerMenuItem2">PRIVACY</a>
+                            <a class="footerSlash">|</a>
+                            <a class="footerMenuItem3">OUR TEAMS</a>
+                        </div>
+                        <p class="footerAddingText">Copyright by phototime - all right reserved</p>
+                    </div>
+                    <div class="footercamera" >
+                    <img src="./img/camera2.svg">
+                    </div>
+                </div>
+            </footer>
+
+        </body>
+        </html>
